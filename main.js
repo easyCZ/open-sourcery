0969256680217b329821c4a55709fe2274e4d1ad
Milan Pavlik <pavlik.mil@gmail.com>
2016-10-30 17:02:51 +0000

    First commit

--- main.js
+++ main.js
@@ -16,16 +16,6 @@ import ReactDOM from 'react-dom';
 import FastClick from 'fastclick';
 import { Provider } from 'react-redux';
 
-import store from './core/store';
-import router from './core/router';
-import history from './core/history';
-
-let routes = require('./routes.json'); // Loaded with utils/routes-loader.js
-const container = document.getElementById('container');
-
-function renderComponent(component) {
-  ReactDOM.render(<Provider store={store}>{component}</Provider>, container);
-}
 
 // Find and render a web page matching the current URL path,
 // if such page is not found then render an error page (see routes.json, core/router.js)
