d305c564c7d11f13daf43430ab746cb3bde31075
Milan Pavlik <pavlik.mil@gmail.com>
2017-02-12 21:04:53 +0000

    Refactor to use SemanticUI

--- src/App.js
+++ src/App.js
@@ -7,6 +7,77 @@ import reactImage from './logos/logo.svg';
 import twitterImage from './logos/Twitter.svg';
 import bloomberg from './logos/Bloomberg_Business.svg';
 
+import { Button, Card, Image } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react'
+
+
+const repos = [
+  {
+    img: require('./logos/freecodecamp.svg'),
+    owner: 'freeCodeCamp',
+    name: 'freeCodeCamp',
+    'description': 'The https://freeCodeCamp.com open source codebase and curriculum. Learn to code and help nonprofits.',
+    language: 'JavaScript',
+    stars: 231796
+  },
+  {
+    img: require('./logos/bootstrap.svg'),
+    owner: 'twbs',
+    name: 'bootstrap',
+    'description': 'The most popular HTML, CSS, and JavaScript framework for developing responsive, mobile first projects on the web.',
+    language: 'JavaScript',
+    stars: 106551
+  },
+  {
+    img: require('./logos/missing.png'),
+    owner: 'vhf',
+    name: 'free-programming-books',
+    'description': '📚 Freely available programming books',
+    language: null,
+    stars: 77766
+  },
+  {
+    img: require('./logos/d3.svg'),
+    owner: 'd3',
+    name: 'd3',
+    'description': 'Bring data to life with SVG, Canvas and HTML. 📊📈🎉',
+    language: 'JavaScript',
+    stars: 60172
+  },
+  {
+    owner: 'facebook',
+    name: 'react',
+    img: reactImage,
+    'description': 'A declarative, efficient, and flexible JavaScript library for building user interfaces.',
+    language: 'JavaScript',
+    stars: 59455
+  },
+  {
+    owner: 'angular',
+    name: 'angular.js',
+    img: require('./logos/angular-icon.svg'),
+    'description': 'AngularJS - HTML enhanced for web apps!',
+    language: 'JavaScript',
+    stars: 54664
+  }
+]
+
+
+const CardExampleGroups = () => (
+  <Card.Group style={{ margin: '0.5em' }}>
+    {repos.map(repo =>
+      <ProjectCard
+        img={repo.img}
+        name={repo.name}
+        owner={repo.owner}
+        description={repo.description}
+        stars={repo.stars}
+        language={repo.language}
+      />)
+    }
+  </Card.Group>
+)
+
 
 class App extends Component {
   render() {
@@ -14,28 +85,7 @@ class App extends Component {
     return (
       <div>
         <Header />
-        <div style={{
-          display: 'flex'
-        }}>
-          <ProjectCard
-            img={reactImage}
-            name="react-native"
-            owner="facebook"
-            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          />
-          <ProjectCard
-            img={twitterImage}
-            name="react-native"
-            owner="facebook"
-            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          />
-          <ProjectCard
-            img={bloomberg}
-            name="react-native"
-            owner="facebook"
-            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          />
-        </div>
+          <CardExampleGroups />
       </div>
     );
   }
--- src/projects/ProjectCard.js
+++ src/projects/ProjectCard.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-import Card from '../card';
+import { Button, Card, Image } from 'semantic-ui-react'
+import { Header } from 'semantic-ui-react'
+import { Icon, Label } from 'semantic-ui-react'
+
 
 const styles = {
   card: {
@@ -40,33 +43,79 @@ const styles = {
 }
 
 
-const ProjectCard = ({ img, owner, name, description }) => (
-  <Card style={styles.card}>
-    <div style={styles.row}>
-
-      <div style={styles.narrowCol}>
-        <img src={img} style={styles.logo} role="presentation" />
-      </div>
-
-      <div style={styles.wideCol}>
-        <h3 style={styles.title}>
-          <span style={styles.owner}>{owner}/</span>
+const ProjectCard = ({ img, owner, name, description, stars, language }) => (
+  <Card style={{ minWidth: '400px' }}>
+    <Card.Content style={{
+      display: 'flex',
+      alignItems: 'center'
+    }}>
+      <Image
+        style={{
+          objectFit: 'cover',
+          margin: '0 1em 0 0'
+        }}
+        size='tiny'
+        floated='left'
+        src={img}
+        width='100px'
+        height='100px'
+        shape='rounded'
+      />
+      <div>
+         <Header size='medium'>
+          <span style={{ color: 'rgba(0, 0, 0, 0.3)' }}>{owner}/</span>
           <span>{name}</span>
-        </h3>
-        <p style={styles.description}>{description}</p>
+        </Header>
+        <p>{description}</p>
       </div>
 
-    </div>
-
-    <div style={styles.row}>
-
-      <div style={styles.meta}>
-        234 members * 14 posts
-      </div>
-
-    </div>
+    </Card.Content>
+    <Card.Content extra>
+      { language ? <Label as='a' basic> {language}</Label> : null }
+      <Label as='a' basic> <Icon name='star' /> {stars}</Label>
+    </Card.Content>
   </Card>
-)
+);
+
+// const ProjectCard =({ img }) => (
+//   <Card
+//     image={img}
+//     header='Elliot Baker'
+//     meta='Friend'
+//     description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+//   />
+// );
+
+
+// const ProjectCard = ({ img, owner, name, description }) => (
+//   <Card style={styles.card}>
+//     <Card.Content>
+//       <div style={styles.row}>
+
+//         <div style={styles.narrowCol}>
+//           <img src={img} style={styles.logo} role="presentation" />
+//         </div>
+
+//         <div style={styles.wideCol}>
+//           <h3 style={styles.title}>
+//             <span style={styles.owner}>{owner}/</span>
+//             <span>{name}</span>
+//           </h3>
+//           <p style={styles.description}>{description}</p>
+//         </div>
+
+//       </div>
+
+//       <div style={styles.row}>
+
+//         <div style={styles.meta}>
+//           234 members * 14 posts
+//         </div>
+
+//       </div>
+//     </Card.Content>
+//   </Card>
+// )
 
 
 export default ProjectCard;
